7d5788ac922cafb728a9d1ba169db8edebf6d8e9
Kundan Gupta <kundankumar8377@gmail.com>
2025-04-27 12:58:00 +0530

    feat: add charts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,30 @@
+import { AppPieChart } from '@/components/custom/PieChart';
+import { AppAreaChart } from '@/components/custom/AreaChart';
+import { AppBarChart } from '@/components/custom/BarChart';
+import { TodoList } from '@/components/custom/TodoList';
+import { CardList } from '@/components/custom/CardList';
+
 export default function HomePage() {
-  return <div className=""></div>;
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-0">
+      <div className="bg-primary-foreground p-4 rounded-lg overflow-auto  lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppBarChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg overflow-auto">
+        <CardList title="Latest Transactions" />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg overflow-auto">
+        <AppPieChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg overflow-auto">
+        <TodoList />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg overflow-auto  lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppAreaChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg overflow-auto">
+        <CardList title="Popular Content" />
+      </div>
+    </div>
+  );
 }
---
+++ src/components/custom/BarChart.tsx
@@ -0,0 +1,61 @@
+'use client';
+
+import React from 'react';
+import { Bar, BarChart, CartesianGrid, XAxis } from 'recharts';
+import {
+  ChartContainer,
+  ChartLegend,
+  ChartLegendContent,
+  ChartTooltip,
+  ChartTooltipContent,
+  type ChartConfig,
+} from '@/components/ui/chart';
+import { Card, CardContent, CardHeader, CardTitle } from '../ui/card';
+
+const chartData = [
+  { month: 'January', desktop: 186, mobile: 80 },
+  { month: 'February', desktop: 305, mobile: 200 },
+  { month: 'March', desktop: 237, mobile: 120 },
+  { month: 'April', desktop: 73, mobile: 190 },
+  { month: 'May', desktop: 209, mobile: 130 },
+  { month: 'June', desktop: 214, mobile: 140 },
+];
+
+const chartConfig = {
+  desktop: {
+    label: 'Desktop',
+    color: 'var(--chart-2)',
+  },
+  mobile: {
+    label: 'Mobile',
+    color: 'var(--chart-3)',
+  },
+} satisfies ChartConfig;
+
+export function AppBarChart() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-xl">Total Revenue</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
+          <BarChart accessibilityLayer data={chartData}>
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="month"
+              tickLine={false}
+              tickMargin={10}
+              axisLine={false}
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <ChartTooltip content={<ChartTooltipContent />} />
+            <ChartLegend content={<ChartLegendContent />} />
+            <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
+            <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
+          </BarChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
+}
